3804b26f166ebb962edb2795968943541da3738e
0xoopi <118335386+0xoopi@users.noreply.github.com>
2023-02-14 09:52:21 +0800

    2 files modified

--- src/hooks/web/useMessage.tsx
+++ src/hooks/web/useMessage.tsx
@@ -1,16 +1,16 @@
-import { ElMessageBox, ElMessage } from 'element-plus';
+// import { ElMessageBox, ElMessage } from 'element-plus';
 
-export function useMessage() {
-  return {
-    createMessage: ElMessage,
-    createConfirm: ElMessageBox.confirm
-    // createSuccessModal,
-    // createErrorModal,
-    // createInfoModal,
-    // createWarningModal,
-  };
-}
+// export function useMessage() {
+//   return {
+//     createMessage: ElMessage,
+//     createConfirm: ElMessageBox.confirm
+//     // createSuccessModal,
+//     // createErrorModal,
+//     // createInfoModal,
+//     // createWarningModal,
+//   };
+// }
 
-export const createMessage = ElMessage
+// export const createMessage = ElMessage
 
-export const createConfirm = ElMessageBox.confirm
+// export const createConfirm = ElMessageBox.confirm
